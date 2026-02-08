3db3a113abc1e563202d5169a76b3108491a9ab6
Lucky <lucyxd480@gmail.com>
2026-02-08 13:18:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
-import os
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-ROUND_TIME = 60
-VOTE_TIME = 45
-MAX_ROUNDS = 7
