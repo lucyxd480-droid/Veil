8cc6c9a71e668a84cfc96c410ee16f401e7b6f36
Lucky <lucyxd480@gmail.com>
2026-02-09 03:38:59 +0530

    Update keyboards.py

--- utils/keyboards.py
+++ utils/keyboards.py
@@ -2,12 +2,12 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def join_keyboard():
     return InlineKeyboardMarkup([
-        [InlineKeyboardButton("✅ Join Game", callback_data="join_game")]
+        [InlineKeyboardButton("Join Game", callback_data="join_game")]
     ])
 
 def dm_options_keyboard():
     return InlineKeyboardMarkup([
-        [InlineKeyboardButton("💗 Trust", callback_data="c_trust")],
-        [InlineKeyboardButton("💔 Betray", callback_data="c_betray")],
-        [InlineKeyboardButton("🖤 Remain Silent", callback_data="c_silent")]
+        [InlineKeyboardButton("Trust", callback_data="c_trust")],
+        [InlineKeyboardButton("Betray", callback_data="c_betray")],
+        [InlineKeyboardButton("Remain Silent", callback_data="c_silent")]
     ])
