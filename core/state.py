64423379df43de3c3695d2e339fc74024d39280a
Lucky <lucyxd480@gmail.com>
2026-02-09 02:16:09 +0530

    Update state.py

--- core/state.py
+++ core/state.py
@@ -6,16 +6,16 @@ class GameState:
 
     def reset(self):
         self.chat_id = None
-        self.players = {}        # user_id: username
+        self.players = {}        
         self.join_open = False
-        self.join_duration = 60  # 60 sec default
+        self.join_duration = 60
         self.join_end_time = None
         self.join_extended = 0
         self.phase = "idle"
         self.round = 0
-        self.choices = {}        # user_id: choice
-        self.votes = {}          # user_id: vote target
-        self.influence = {}      # user_id: influence points
+        self.choices = {}        
+        self.votes = {}          
+        self.influence = {}      
         self.trust_collapse = 0
         self.max_rounds = 5
 
