d67aa4e648a12a65d7440b6f5d7b3f5024ad0b2e
Lucky <lucyxd480@gmail.com>
2026-02-08 15:43:45 +0530

    Update state.py

--- core/state.py
+++ core/state.py
@@ -1,27 +1,21 @@
 import time
-import asyncio
 
 class GameState:
     def __init__(self):
         self.reset()
 
     def reset(self):
-        # group info
         self.chat_id = None
 
-        # players
         self.players = {}  # user_id: name
 
-        # join phase
         self.join_open = False
         self.join_duration = 30
         self.extend_duration = 15
         self.join_end_time = None
         self.extended = False
-        self.join_task = None
 
-        # game
-        self.round = 0
         self.phase = "idle"
+        self.round = 0
 
 game = GameState()
