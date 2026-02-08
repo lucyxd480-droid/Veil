ed7ae5a0d7abe43cec94472fceee9e7b8d7961ac
Lucky <lucyxd480@gmail.com>
2026-02-08 15:49:30 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,27 +1,29 @@
 from pyrogram import filters
+import asyncio
+
 from core.state import game
 from utils.keyboards import join_keyboard
 from utils.text import START_TEXT
 from handlers.join_timer import start_join_timer
-import asyncio
 
 
 def register_start(app):
 
     @app.on_message(filters.group & filters.command("start"))
-    async def start_group(client, message):
+    async def start_group(_, msg):
         if game.join_open:
-            await message.reply("🕯 A game is already forming.")
+            await msg.reply("🕯 A game is already forming.")
             return
 
         game.reset()
         game.join_open = True
+        game.chat_id = msg.chat.id
 
-        await message.reply(
+        await msg.reply(
             START_TEXT,
             reply_markup=join_keyboard()
         )
 
         asyncio.create_task(
-            start_join_timer(app, message.chat.id)
+            start_join_timer(app, msg.chat.id)
         )
