a92214aa102480d4ad4082a05bd35530f43b2242
Lucky <lucyxd480@gmail.com>
2026-02-08 09:59:57 +0530

    Update state.py

--- core/state.py
+++ core/state.py
@@ -1,9 +1,20 @@
+import time
+
 class GameState:
     def __init__(self, chat_id):
         self.chat_id = chat_id
+
+        # lobby
+        self.players = {}
+        self.join_open = True
+        self.join_duration = 30
+        self.extend_duration = 15
+        self.join_end_time = time.time() + self.join_duration
+        self.extended = False
+
+        # game
         self.active = False
         self.round = 0
-        self.players = {}
+        self.phase = "join"
         self.choices = {}
         self.votes = {}
-        self.phase = "idle"  # idle, decision, voting
