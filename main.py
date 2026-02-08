469e805d700c84ad528c2f3a10cb7736a2d5ecf6
Lucky <lucyxd480@gmail.com>
2026-02-08 12:00:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
-from handlers.admin import register_admin
+from config import BOT_TOKEN, API_ID, API_HASH
+
 from handlers.join import register_join
-from handlers.callbacks import register_callbacks
+from handlers.start import register_start
+from handlers.admin import register_admin
 
-app = Client("veil_bot", API_ID, API_HASH, bot_token=BOT_TOKEN)
 
-register_admin(app)
+app = Client(
+    "veil",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
 register_join(app)
-register_callbacks(app)
+register_start(app)
+register_admin(app)
 
 app.run()
-
