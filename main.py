7b98122debdeb58ed43a7419cad1f77ec718fc1d
Lucky <lucyxd480@gmail.com>
2026-02-09 01:30:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from handlers.start import register_start
 from handlers.dm_join import register_dm_join
 from handlers.admin import register_admin
 from handlers.begin import register_begin
-
+from handlers.dm_round import register_dm_round
+from handlers.voting import register_voting
 
 app = Client(
     "veil",
@@ -18,5 +19,7 @@ register_start(app)
 register_dm_join(app)
 register_admin(app)
 register_begin(app)
+register_dm_round(app)
+register_voting(app)
 
 app.run()
