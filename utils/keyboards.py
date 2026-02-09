aef926bcdb7f558c5a65a9b3fc35954f067dcf08
Lucky <lucyxd480@gmail.com>
2026-02-09 07:35:16 +0530

    Update keyboards.py

--- utils/keyboards.py
+++ utils/keyboards.py
@@ -1,22 +1,19 @@
-diff --git a/utils/keyboards.py b/utils/keyboards.py
-index d0ab6d2b5befe00cb8b82848c2fbe34704cf4003..6d7e9bf8cbdd04e650c95336daceca3a47513cc5 100644
---- a/utils/keyboards.py
-+++ b/utils/keyboards.py
-@@ -1,13 +1,15 @@
- from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
- 
--def join_keyboard():
-+
-+def join_keyboard(bot_username: str):
-     return InlineKeyboardMarkup([
--        [InlineKeyboardButton("Join Game", callback_data="join_game")]
-+        [InlineKeyboardButton("Join The Veil", url=f"https://t.me/@Veiltestrobot?start=join")]
-     ])
- 
-+
- def dm_options_keyboard():
-     return InlineKeyboardMarkup([
-         [InlineKeyboardButton("Trust", callback_data="c_trust")],
-         [InlineKeyboardButton("Betray", callback_data="c_betray")],
-         [InlineKeyboardButton("Remain Silent", callback_data="c_silent")]
-     ])
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+BOT_USERNAME = "Veiltestrobot"  # your bot username
+
+def join_keyboard():
+    return InlineKeyboardMarkup([
+        [InlineKeyboardButton(
+            "Join The Veil",
+            url=f"https://t.me/{BOT_USERNAME}?start=join"
+        )]
+    ])
+
+
+def dm_options_keyboard():
+    return InlineKeyboardMarkup([
+        [InlineKeyboardButton("Trust", callback_data="c_trust")],
+        [InlineKeyboardButton("Betray", callback_data="c_betray")],
+        [InlineKeyboardButton("Remain Silent", callback_data="c_silent")]
+    ])
