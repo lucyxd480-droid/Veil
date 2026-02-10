9df270b5064e38ddfec0e5099266b0b8a508dcb6
Lucky <lucyxd480@gmail.com>
2026-02-11 02:29:49 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,22 +1,20 @@
 from pyrogram import filters
 from core.state import games
+from core.engine import assign_roles
 from utils.keyboards import join_kb
-from config import JOIN_TIME
-import asyncio
-
-
-
 
 def register_start(app):
-@app.on_message(filters.command("startgame") & filters.group)
-async def start(_, msg):
-chat = msg.chat.id
-games[chat].clear()
-games[chat]["players"] = {}
-games[chat]["phase"] = "join"
+    @app.on_message(filters.command("startgame") & filters.group)
+    async def start(_, msg):
+        chat = msg.chat.id
 
+        games[chat].clear()
+        games[chat]["players"] = {}
+        games[chat]["roles"] = {}
+        games[chat]["alive"] = set()
+        games[chat]["phase"] = "join"
 
-await msg.reply(
-"🕯 THE VEIL OPENS...\nJoin within 60 seconds.",
-reply_markup=join_kb()
-)
+        await msg.reply(
+            "🕯 THE VEIL OPENS...\nJoin within 60 seconds.",
+            reply_markup=join_kb()
+        )
