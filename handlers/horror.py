d43dbf840008271157378f1e11db94046af527a9
Lucky <lucyxd480@gmail.com>
2026-02-11 02:04:07 +0530

    Create horror.py

---
+++ handlers/horror.py
@@ -0,0 +1,10 @@
+from pyrogram import filters
+from core.horror import horror
+
+
+
+
+def register_horror(app):
+@app.on_message(filters.command("horror") & filters.group)
+async def horror_mode(_, msg):
+await msg.reply(f"🧠 {horror()}")
