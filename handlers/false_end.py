ec16fdfa7b341572afa67fd594078c498167ffff
Lucky <lucyxd480@gmail.com>
2026-02-11 02:07:47 +0530

    Create false_end.py

---
+++ handlers/false_end.py
@@ -0,0 +1,13 @@
+from pyrogram import filters
+from core.false_end import false_end
+
+
+
+
+def register_false_end(app):
+@app.on_message(filters.command("checkend") & filters.group)
+async def check(_, msg):
+if false_end():
+await msg.reply("🕯 GAME OVER... or is it?")
+else:
+await msg.reply("The game continues.")
