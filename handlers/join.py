aaed9bbc74c91bc5cbe028c89c6183071e372ecc
Lucky <lucyxd480@gmail.com>
2026-02-11 01:30:04 +0530

    Update join.py

--- handlers/join.py
+++ handlers/join.py
@@ -1,20 +1,18 @@
 from pyrogram import filters
 from core.state import games
-from utils.keyboards import enter_kb
-from config import MIN_PLAYERS
+
 
 def register_join(app):
-    @app.on_callback_query(filters.regex("^join$"))
-    async def join(_, cq):
-        game = games.get(cq.message.chat.id)
-        if not game:
-            return
+@app.on_callback_query(filters.regex("^join$"))
+async def join(_, cb):
+chat = cb.message.chat.id
+user = cb.from_user
+game = games[chat]
 
-        uid = cq.from_user.id
-        game.players[uid] = cq.from_user.first_name
-        game.alive.add(uid)
 
-        await cq.answer("Joined 🕯", show_alert=True)
+if user.id in game["players"]:
+return await cb.answer("Already joined", show_alert=True)
 
-        if len(game.players) == MIN_PLAYERS:
-            await cq.message.reply("🕯 Darkness ready.", reply_markup=enter_kb())
+
+game["players"][user.id] = user.first_name
+await cb.answer("Joined")
