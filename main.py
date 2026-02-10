a0ce4585727bb1df68b9aa42e561cc98d9f0e596
Lucky <lucyxd480@gmail.com>
2026-02-10 13:17:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
-
-from handlers.start import register_start
-from handlers.dm_join import register_dm_join
-from handlers.admin import register_admin
 from handlers.begin import register_begin
-from handlers.dm_round import register_dm_round
-from handlers.voting import register_voting
+from handlers.join import register_join
+from handlers.veil import register_veil
+from handlers.leaderboard import register_leaderboard
 
 app = Client(
-    "veil",
+    "veil_v45",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
 
-register_start(app)
-register_dm_join(app)
-register_admin(app)
 register_begin(app)
-register_dm_round(app)
-register_voting(app)
+register_join(app)
+register_veil(app)
+register_leaderboard(app)
 
 app.run()
