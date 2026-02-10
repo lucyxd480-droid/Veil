8bc716fba3a0177ca36ec56a51b4fc1eeca93507
Lucky <lucyxd480@gmail.com>
2026-02-11 02:46:08 +0530

    Update engine.py

--- core/engine.py
+++ core/engine.py
@@ -3,15 +3,16 @@ from core.roles import ROLES, EVIL
 
 
 def assign_roles(players):
-roles = list(ROLES.keys())
-random.shuffle(roles)
-assigned = {}
-evil_count = max(1, len(players)//4)
+    roles = list(ROLES.keys())
+    random.shuffle(roles)
 
+    assigned = {}
+    evil_count = max(1, len(players) // 4)
 
-for i, user in enumerate(players):
-if i < evil_count:
-assigned[user] = random.choice(list(EVIL))
-else:
-assigned[user] = "innocent"
-return assigned
+    for i, user_id in enumerate(players):
+        if i < evil_count:
+            assigned[user_id] = random.choice(list(EVIL))
+        else:
+            assigned[user_id] = "innocent"
+
+    return assigned
