7485eafda1b73765a906d5fa61b163f0369a1649
Lucky <lucyxd480@gmail.com>
2026-02-11 01:47:14 +0530

    Create roles.py

---
+++ core/roles.py
@@ -0,0 +1,15 @@
+ROLES = {
+"traitor": "🩸 Kill at night",
+"assassin": "🗡 One extra kill",
+"cultist": "🕷 Convert others",
+"doppelganger": "🪞 Copy role",
+"puppeteer": "🕸 Control pick",
+"zealot": "🕯 Die to win",
+"mindbreaker": "🧠 Cause insanity",
+"guardian": "🛡 Protect one",
+"watcher": "👁 Observe actions",
+"innocent": "🤍 Survive"
+}
+
+
+EVIL = {"traitor","assassin","cultist","puppeteer","mindbreaker","zealot"}
