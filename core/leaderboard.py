3af0eb44a9e626cfca8fcdbb5d20282b711a8668
Lucky <lucyxd480@gmail.com>
2026-02-11 01:42:46 +0530

    Update leaderboard.py

--- core/leaderboard.py
+++ core/leaderboard.py
@@ -1,7 +1,20 @@
-leaderboard = {}
+import json
+import os
 
-def add_win(uid):
-    leaderboard[uid] = leaderboard.get(uid, 0) + 1
 
-def top_players():
-    return sorted(leaderboard.items(), key=lambda x: x[1], reverse=True)
+PATH = "data/leaderboard.json"
+
+
+if not os.path.exists(PATH):
+with open(PATH, "w") as f:
+json.dump({}, f)
+
+
+
+
+def add_win(user_id):
+with open(PATH) as f:
+data = json.load(f)
+data[str(user_id)] = data.get(str(user_id), 0) + 1
+with open(PATH, "w") as f:
+json.dump(data, f, indent=2)
