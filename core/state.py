7c1b574a7bc7bccb9da5e335bb4e748c83868904
Lucky <lucyxd480@gmail.com>
2026-02-10 13:20:38 +0530

    Update state.py

--- core/state.py
+++ core/state.py
@@ -1,38 +1,18 @@
-import time
-
 class GameState:
-    def __init__(self):
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
         self.reset()
 
     def reset(self):
-        self.chat_id = None
-        self.host_id = None
-        self.host_name = None
-
-        self.players = {}          # user_id: display_name
-        self.influence = {}        # user_id: points
-        self.usernames = {}        # user_id: username or None
-
-        self.join_open = False
-        self.join_duration = 60
-        self.join_end_time = None
-        self.join_reminders_sent = set()
-
-        self.phase = "idle"        # idle | join | ready | round | voting
+        self.players = {}
+        self.alive = set()
+        self.roles = {}
+        self.choices = {}
+        self.votes = {}
         self.round = 0
+        self.round_lock = False
+        self.active = False
+        self.guardian_used = False
+        self.shadow_used = set()
 
-        self.choices = {}          # user_id: trust|betray|silent
-        self.choice_deadline = None
-
-        self.votes = {}            # voter_id: target_id
-        self.vote_deadline = None
-
-        self.trust_collapse = 0
-        self.max_rounds = 5
-
-    def now(self):
-        return time.time()
-
-
-# single instance of game state
-game = GameState()
+games = {}
