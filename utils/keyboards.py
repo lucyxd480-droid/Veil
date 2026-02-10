e23888e63ad498067843f17cf4c921d15f74d2a8
Lucky <lucyxd480@gmail.com>
2026-02-11 02:50:00 +0530

    Update keyboards.py

--- utils/keyboards.py
+++ utils/keyboards.py
@@ -2,13 +2,17 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def join_kb():
-return InlineKeyboardMarkup([
-[InlineKeyboardButton("🩸 Join Game", callback_data="join")]
-])
+    return InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("🩸 Join Game", callback_data="join")]
+        ]
+    )
 
 
 def pick_kb(players):
-return InlineKeyboardMarkup([
-[InlineKeyboardButton(p, callback_data=f"pick:{p}")]
-for p in players
-])
+    return InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(p, callback_data=f"pick:{p}")]
+            for p in players
+        ]
+    )
