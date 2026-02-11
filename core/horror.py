8aa45d10a0abfd609e90a1380af7db9f092af4d0
Lucky <lucyxd480@gmail.com>
2026-02-11 06:18:34 +0530

    Update horror.py

--- core/horror.py
+++ core/horror.py
@@ -2,12 +2,12 @@ import random
 
 
 HORROR_LINES = [
-"You feel watched.",
-"Your thoughts are not yours.",
-"Someone knows your secret.",
-"The darkness remembers you."
+    "You feel watched.",
+    "Your thoughts are not yours.",
+    "Someone knows your secret.",
+    "The darkness remembers you.",
 ]
 
 
 def horror():
-return random.choice(HORROR_LINES)
+    return random.choice(HORROR_LINES)
